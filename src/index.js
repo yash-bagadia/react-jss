ec281a9e2f9584e2d80cf97acf64babc0af50a67
Oleg Slobodskoi <oleg008@gmail.com>
2016-08-01 16:06:15 +0200

    update dev deps, jss 5, remove babel 5 compatibility
    cssinjs/jss#249

--- src/index.js
+++ src/index.js
@@ -92,6 +92,3 @@ export default function useSheet(DecoratedComponent, rules, options) {
 
   return (_DecoratedComponent) => decorate(_DecoratedComponent, rules, options, customJss)
 }
-
-// Hotfix for babel 5 migration, will be removed in the next major version.
-module.exports = exports = useSheet
