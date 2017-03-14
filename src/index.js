902f2c8d07091441df1a8e816e0bffd700ea5f7b
Oleg Slobodskoi <oleg008@gmail.com>
2017-03-14 10:42:08 +0200

    convert comment to multiline

--- src/index.js
+++ src/index.js
@@ -89,14 +89,18 @@ const Container = ({children}) => (children || null)
 
 export const jss = createJss(preset())
 
-// Global index counter to preserve source order.
-// As we create the style sheet during componentWillMount lifecycle,
-// children are handled after the parents, so the order of style elements would
-// be parent->child. It is a problem though when a parent passes a className
-// which needs to override any childs styles. StyleSheet of the child has a higher
-// specificity, because of the source order.
-// So our solution is to render sheets them in the reverse order child->sheet, so
-// that parent has a higher specificity.
+/**
+ * Global index counter to preserve source order.
+ * As we create the style sheet during componentWillMount lifecycle,
+ * children are handled after the parents, so the order of style elements would
+ * be parent->child. It is a problem though when a parent passes a className
+ * which needs to override any childs styles. StyleSheet of the child has a higher
+ * specificity, because of the source order.
+ * So our solution is to render sheets them in the reverse order child->sheet, so
+ * that parent has a higher specificity.
+ *
+ * @type {Number}
+ */
 let indexCounter = -100000
 
 /**
