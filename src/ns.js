fd3e65740c3aad38271ffb5e1ebdec22e95764f0
Oleg Slobodskoi <oleg008@gmail.com>
2017-07-12 18:19:58 +0200

    use constant namespaces to allow interoperability between multiple versions

--- src/ns.js
+++ src/ns.js
@@ -1,4 +1,7 @@
-export const jss = Math.random()
-export const sheetsRegistry = Math.random()
-export const sheetsManager = Math.random()
-export const sheetOptions = Math.random()
+/**
+ * Namespaces to avoid conflicts on the context.
+ */
+export const jss = '64a55d578f856d258dc345b094a2a2b3'
+export const sheetsRegistry = 'd4bd0baacbc52bbd48bbb9eb24344ecd'
+export const sheetsManager = 'f987ce8a891ac5ccdb9b072fa339e6c1'
+export const sheetOptions = '6fc570d6bd61383819d0f9e7407c452d'
