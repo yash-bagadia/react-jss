55cb5ae4afc600605acb62bd0c8470375853ad0e
Oleg Slobodskoi <oleg008@gmail.com>
2017-11-11 19:34:10 +0100

    use public api from jss

--- src/jss.js
+++ src/jss.js
@@ -1,7 +1,11 @@
 import {create} from 'jss'
 import preset from 'jss-preset-default'
 
-export {default as createGenerateClassNameDefault} from 'jss/lib/utils/createGenerateClassName'
-export {default as SheetsManager} from 'jss/lib/SheetsManager'
-export {SheetsRegistry, getDynamicStyles} from 'jss'
+export {
+  SheetsRegistry,
+  getDynamicStyles,
+  SheetsManager,
+  createGenerateClassName as createGenerateClassNameDefault
+} from 'jss'
+
 export default create(preset())
